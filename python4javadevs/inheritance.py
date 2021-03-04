de682c9b0f6ef6847016dd9df3ad8cbfc0b85b15
Phillip Henry <PhillHenry@gmail.com>
2021-03-04 10:15:23 +0000

    __new__

--- python4javadevs/inheritance.py
+++ python4javadevs/inheritance.py
@@ -1,4 +1,17 @@
 class ParentA:
+
+    # Use __new__ when you need to control the creation of a new instance.
+    #
+    # Use __init__ when you need to control initialization of a new instance.
+    #
+    # __new__ is the first step of instance creation. It's called first, and is responsible for returning a new instance of your class.
+    #
+    # In contrast, __init__ doesn't return anything; it's only responsible for initializing the instance after it's been creat
+    # https://stackoverflow.com/questions/674304/why-is-init-always-called-after-new
+    def __new__(cls):
+        print("NEW")
+        return super(ParentA, cls).__new__(cls)
+
     def __init__(self):
         log("ParentA: init")
 
